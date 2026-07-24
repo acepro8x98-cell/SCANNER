cdba1f0e775de629244ead1bb055cce63d70d9c0
acepro8x98-cell <acepro8x98@gmail.com>
2026-07-24 18:16:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // ==== CẤU HÌNH ====
 // Dán URL Web App của Google Apps Script vào đây sau khi Deploy (xem HUONG_DAN.md)
 // Ví dụ: "https://script.google.com/macros/s/AKfycb.../exec"
-const WEBAPP_URL = "https://script.google.com/macros/s/PASTE_YOUR_DEPLOYMENT_ID_HERE/exec";
+const WEBAPP_URL = "https://script.google.com/macros/s/https://script.google.com/macros/s/AKfycbyWl04QcmQGRrk4df9MULweMyFrYng4mDy4bcI3gRvxDtzYpaoG4KYQwLCFGvGRKTku/exec";
